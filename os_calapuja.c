60f07816bfc28db0a50f33dfe4eb4bed354150da
root <root@localhost.localdomain>
2022-08-01 20:43:55 -0500

    mi commit 1

---
+++ os_calapuja.c
@@ -0,0 +1,19 @@
+lude<stdio.h>
+
+int main(){
+
+printf("hola liz");
+return 0;
+lude<stdio.h>
+
+int main(){
+
+printf("hola liz");
+return 0;
+lude<stdio.h>
+
+int main(){
+
+printf("hola liz");
+return 0;
+}
---
+++ procesos/proceso.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+
+printf("hola liz");
+return 0;
+}
